6afd72959af5e9ec93571c9eb4034b7725666876
byrontung <btung1@ualberta.ca>
2021-08-11 09:27:14 -0600

    Scrollable is created using CSS but works only for touchscreen

--- src/App.js
+++ src/App.js
@@ -7,6 +7,11 @@ function App() {
       <Movie name="Movie 1" />
       <Movie name="Movie 2" />
       <Movie name="Movie 3" />
+      <Movie name="Movie 4" />
+      <Movie name="Movie 5" />
+      <Movie name="Movie 6" />
+      <Movie name="Movie 7" />
+      <Movie name="Movie 8" />
     </div>
   );
 }
---
+++ src/components/scrollable.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Scrollable = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Scrollable
