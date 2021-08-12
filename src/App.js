dfb767699b9ad24a4ab834fa8eefe7e6ded1eecf
byrontung <btung1@ualberta.ca>
2021-08-12 08:18:23 -0600

    Carousel implemented. Need to discuss how Movie components are stored

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import './App.css';
 import Movie from './components/Movie';
+import Scrollable from './components/Scrollable';
 
 function App() {
   return (
-    <div className="Scrollable">
-      <Movie name="Movie 1" />
-      <Movie name="Movie 2" />
-      <Movie name="Movie 3" />
-      <Movie name="Movie 4" />
-      <Movie name="Movie 5" />
-      <Movie name="Movie 6" />
-      <Movie name="Movie 7" />
-      <Movie name="Movie 8" />
+    <div>
+      <div className="Scrollable">
+        <Movie name="Movie 1" />
+        <Movie name="Movie 2" />
+        <Movie name="Movie 3" />
+        <Movie name="Movie 4" />
+        <Movie name="Movie 5" />
+        <Movie name="Movie 6" />
+        <Movie name="Movie 7" />
+        <Movie name="Movie 8" />
+      </div>
+      <Scrollable/>
     </div>
   );
 }
--- src/components/scrollable.js
+++ src/components/scrollable.js
@@ -1,11 +1,41 @@
 import React from 'react'
+import Movie from './Movie';
+import Carousel, { slidesToShowPlugin } from '@brainhubeu/react-carousel';
+import '@brainhubeu/react-carousel/lib/style.css';
 
+const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  };
 const Scrollable = () => {
     return (
         <div>
-            
+            <h2> Single Item</h2>
+            <Carousel
+                plugins={[
+                    'arrows',
+                    {
+                    resolve: slidesToShowPlugin,
+                    options: {
+                    numberOfSlides: 3
+                    }
+                    },
+                ]}
+              >
+                <Movie name="Movie 1" />
+                <Movie name="Movie 2" />
+                <Movie name="Movie 3" />
+                <Movie name="Movie 4" />
+                <Movie name="Movie 5" />
+                <Movie name="Movie 6" />
+                <Movie name="Movie 7" />
+                <Movie name="Movie 8" />
+            </Carousel>
         </div>
-    )
+    );
 }
 
 export default Scrollable
