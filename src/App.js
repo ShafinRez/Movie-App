d68e900b0353c8fb205deedd84127e7a8ad796dd
byrontung <btung1@ualberta.ca>
2021-08-13 23:39:01 -0600

    Added responsiveness with 'react-responsive' package, double check if this is the proper way of using MediaQuery.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
 import './App.css';
-import Movie from './components/Movie';
 import Scrollable from './components/Scrollable';
 
 function App() {
   return (
     <div>
-      <div className="Scrollable">
-        <Movie name="Movie 1" />
-        <Movie name="Movie 2" />
-        <Movie name="Movie 3" />
-        <Movie name="Movie 4" />
-        <Movie name="Movie 5" />
-        <Movie name="Movie 6" />
-        <Movie name="Movie 7" />
-        <Movie name="Movie 8" />
-      </div>
       <Scrollable/>
     </div>
   );
