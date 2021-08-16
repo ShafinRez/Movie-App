80af173d2deba824f5d06f815f500424afc1e5d0
byrontung <btung1@ualberta.ca>
2021-08-15 23:19:28 -0700

    Found a better to implement responsive Carousel. May need to add breakpoints for mobile.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
+import React from 'react';
 import './App.css';
 import Scrollable from './components/Scrollable';
 
 function App() {
-  return (
-    <div>
-      <Scrollable/>
-    </div>
-  );
+	return (
+		<div>
+			<Scrollable />
+		</div>
+	);
 }
 
 export default App;
--- src/components/scrollable.js
+++ src/components/scrollable.js
@@ -1,68 +1,55 @@
 import React from 'react'
 import Movie from './Movie';
 import Carousel, { slidesToShowPlugin } from '@brainhubeu/react-carousel';
-import MediaQuery, { useMediaQuery } from 'react-responsive';
 import '@brainhubeu/react-carousel/lib/style.css';
 
 const Scrollable = () => {
 
-    const isDesktopOrLaptop = useMediaQuery({ minWidth: 1224 })
-    const isBigScreen = useMediaQuery({ minWidth: 1824 })
-    const isTabletOrMobile = useMediaQuery({ maxWidth: 1224 })
-    const isPortrait = useMediaQuery({ orientation: 'portrait' })
-    const isRetina = useMediaQuery({ minResolution: '2dppx' })
-    
-    return (
-        <div>
-            <h2> Top Hits </h2>
-            <MediaQuery minWidth={1224}>
-                <Carousel
-                    plugins={[
-                        'arrows',
-                        {
-                        resolve: slidesToShowPlugin,
-                        options: {
-                        numberOfSlides: 5                        
-                        }
-                        },
-                    ]}
-                >
-                    {/* put all in ul, each Movie is in an li, change css ul in className*/}
-                    <Movie name="Movie 1" />
-                    <Movie name="Movie 2" />
-                    <Movie name="Movie 3" />
-                    <Movie name="Movie 4" />
-                    <Movie name="Movie 5" />
-                    <Movie name="Movie 6" />
-                    <Movie name="Movie 7" />
-                    <Movie name="Movie 8" />
-                </Carousel>
-            </MediaQuery>
-            <MediaQuery maxWidth={1224}>
-                <Carousel
-                    plugins={[
-                        'arrows',
-                        {
-                        resolve: slidesToShowPlugin,
-                        options: {
-                        numberOfSlides: 3                       
-                        }
-                        },
-                    ]}
-                >
-                    {/* put all in ul, each Movie is in an li, change css ul in className*/}
-                    <Movie name="Movie 1" />
-                    <Movie name="Movie 2" />
-                    <Movie name="Movie 3" />
-                    <Movie name="Movie 4" />
-                    <Movie name="Movie 5" />
-                    <Movie name="Movie 6" />
-                    <Movie name="Movie 7" />
-                    <Movie name="Movie 8" />
-                </Carousel>
-            </MediaQuery>
-        </div>
-    );
+	// const isDesktopOrLaptop = useMediaQuery({ minWidth: 1224 })
+	// const isBigScreen = useMediaQuery({ minWidth: 1824 })
+	// const isTabletOrMobile = useMediaQuery({ maxWidth: 1224 })
+	// const isPortrait = useMediaQuery({ orientation: 'portrait' })
+	// const isRetina = useMediaQuery({ minResolution: '2dppx' })
+
+	return (
+		<div>
+			<h2> Top Hits </h2>
+			<Carousel
+				plugins={[
+					'arrows',
+					{
+						resolve: slidesToShowPlugin,
+						options: {
+							numberOfSlides: 5
+						}
+					},
+				]}
+				breakpoints={{
+					1224: {
+						plugins:
+							[
+								{
+									resolve: slidesToShowPlugin,
+									options: {
+										numberOfSlides: 3
+									}
+								},
+							]
+					}
+				}}
+			>
+				{/* put all in ul, each Movie is in an li, change css ul in className*/}
+				<Movie name="Movie 1" />
+				<Movie name="Movie 2" />
+				<Movie name="Movie 3" />
+				<Movie name="Movie 4" />
+				<Movie name="Movie 5" />
+				<Movie name="Movie 6" />
+				<Movie name="Movie 7" />
+				<Movie name="Movie 8" />
+			</Carousel>
+		</div>
+	);
 }
 
 export default Scrollable
